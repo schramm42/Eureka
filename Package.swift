27f5a6ffecfc1af0ccdd8547418af2f2c3a7b3fe
schramm42 <schramm42@gmail.com>
2019-11-14 13:08:07 +0100

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Eureka",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(name: "Eureka", targets: ["Eureka"])
+    ],
+    targets: [
+        .target(
+            name: "Eureka",
+            path: "Eureka"
+        )
+    ]
+)
